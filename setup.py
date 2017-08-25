9b6a95f99c3c43c5c6de355d9e6c867d88bc3a22
Rafahela <rafahela@gmail.com>
2017-08-25 10:17:11 -0300

    Back to development: 1.0rc4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc3'
+version = '1.0rc4.dev0'
 description = 'Portal Modelo: IntegraĂ§ĂŁo com sistemas do processo legislativo.'
 long_description = (
     open('README.rst').read() + '\n' +
