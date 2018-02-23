7b368a6a63df24a285532f15bb79fa3c3a200ab8
Rafahela <rafahela@gmail.com>
2018-02-23 11:16:07 -0300

    Preparing release 1.0rc6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc6.dev0'
+version = '1.0rc6'
 description = 'Portal Modelo: IntegraĂ§ĂŁo com sistemas do processo legislativo.'
 long_description = (
     open('README.rst').read() + '\n' +
